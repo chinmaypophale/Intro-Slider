8385a67e223c5ab311614dcc48ff498ab3af6ff2
chinmaypophale <37659306+chinmaypophale@users.noreply.github.com>
2018-06-14 20:41:47 +0530

    Intro Slider

---
+++ app/src/main/java/com/dexterlabs/introslider/introsliderandroidhive/MainActivity.java
@@ -0,0 +1,16 @@
+package com.dexterlabs.introslider.introsliderandroidhive;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+    }
+}
